bdd722398db5a6326730c32c0b76df249f19a76e
João Victor Kikuti <joaovictorkikuti@gmail.com>
2021-04-14 11:40:16 -0300

    adicionado função createPalette e createPixels

--- script.js
+++ script.js
@@ -1,10 +1,29 @@
+createPalette();
+createPixels(25);
 
-sla();
-function sla () {
-  const PALETTE_COLORS = document.getElementsByTagName('th');
-  for (let index of PALETTE_COLORS) {
-    console.log(index)
-    console.log(index.className);
-    index.style.backgroundColor = index.className;
+
+function createPalette () {
+  const COLORS = ['black', 'red', 'blue', 'green'];
+  const PALETTE = document.getElementById('color-palette');
+
+  for (let index in COLORS) {
+    const PALETTE_COLORS = document.createElement('div');
+    PALETTE_COLORS.className = 'pixel';
+    PALETTE_COLORS.style.backgroundColor = COLORS[index];
+    PALETTE.appendChild(PALETTE_COLORS);
+  }
+}
+
+
+function createPixels(pixels) {
+  const PIXEL_BOARD = document.getElementById('pixel-board');
+  const SECTION_CONTAINER = document.getElementById('section-container');
+
+  SECTION_CONTAINER.style.width = (40 * Math.sqrt(pixels)) + 'px';
+
+  for (let index = 0; index < pixels; index += 1) {
+    const PIXEL = document.createElement('div');
+    PIXEL.className = 'pixel';
+    PIXEL_BOARD.appendChild(PIXEL);
   }
 }
