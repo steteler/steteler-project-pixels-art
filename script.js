499cbef57723c20223362ff0b7cf2e6bce240206
João Victor Kikuti <joaovictorkikuti@gmail.com>
2021-04-14 12:58:07 -0300

    arrumado erros apontados pelo lint

--- script.js
+++ script.js
@@ -1,25 +1,21 @@
-createPalette();
-createPixels(25);
-
-
-function createPalette () {
-  const COLORS = ['black', 'red', 'blue', 'green'];
+function createPalette(colors) {
   const PALETTE = document.getElementById('color-palette');
 
-  for (let index in COLORS) {
+  for (const index of colors) {
     const PALETTE_COLORS = document.createElement('div');
     PALETTE_COLORS.className = 'pixel';
-    PALETTE_COLORS.style.backgroundColor = COLORS[index];
+    PALETTE_COLORS.style.backgroundColor = index;
     PALETTE.appendChild(PALETTE_COLORS);
   }
 }
 
+createPalette(['black', 'red', 'blue', 'green']);
 
 function createPixels(pixels) {
   const PIXEL_BOARD = document.getElementById('pixel-board');
   const SECTION_CONTAINER = document.getElementById('section-container');
 
-  SECTION_CONTAINER.style.width = (40 * Math.sqrt(pixels)) + 'px';
+  SECTION_CONTAINER.style.width = `${40 * Math.sqrt(pixels)}px`;
 
   for (let index = 0; index < pixels; index += 1) {
     const PIXEL = document.createElement('div');
@@ -27,3 +23,5 @@ function createPixels(pixels) {
     PIXEL_BOARD.appendChild(PIXEL);
   }
 }
+
+createPixels(25);
